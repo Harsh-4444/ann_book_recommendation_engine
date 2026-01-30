c023d2fa99d045b0e24c6e1818f604201023aece
Harshavardhan Shetty <93480906+Harsh-4444@users.noreply.github.com>
2026-01-30 15:02:36 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -3,11 +3,7 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
-tensorflow==2.15
-numpy
-pandas
-scikit-learn
-streamlit
+
 
 
 # Load files
